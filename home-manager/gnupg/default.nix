7ddc2e4715af03e8da750115d89aed425d07920d
Doug Campos <qmx@qmx.me>
2025-10-28 23:41:06 -0400

    Make pinentry platform-agnostic
    - Use pinentry_mac on macOS (Darwin)
    - Use pinentry-curses on Linux for command-line interface
    - Enables core.nix to work on both platforms

--- home-manager/gnupg/default.nix
+++ home-manager/gnupg/default.nix
@@ -10,7 +10,7 @@
     enableZshIntegration = true;
     defaultCacheTtl = 1800;
     maxCacheTtl = 7200;
-    pinentry.package = pkgs.pinentry_mac;
+    pinentry.package = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-curses;
     grabKeyboardAndMouse = false;
     extraConfig = ''
       # Clean configuration without problematic options
