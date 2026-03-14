a6b3bef697dede685b3ad65d5faac715412ac1da
Doug Campos <qmx@qmx.me>
2026-03-14 00:14:25 -0400

    home-manager/tmux: add extended-keys-format csi-u for pi compatibility

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -6,6 +6,7 @@
     baseIndex = 1;
     extraConfig = ''
       set -g extended-keys on
+      set -g extended-keys-format csi-u
       set -g allow-passthrough on
       set -g status-left '[#S] #h '
       set -g status-left-length 20
