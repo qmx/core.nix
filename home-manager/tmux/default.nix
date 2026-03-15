de1a6d8718011c445f3ed6af9cda0c60e10851b7
Doug Campos <qmx@qmx.me>
2026-03-14 23:35:31 -0400

    fix: use correct CSI-u modifier (3) for Shift+Enter
    Ghostty sends modifier 3 for Shift+Enter, not modifier 2.
    Updated both Ghostty config and tmux bind-key to use \e[13;3u

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -14,7 +14,7 @@
       # Explicitly bind Shift+Enter to send CSI-u sequence
       # This is required because tmux only forwards extended keys to panes
       # that request them via Kitty activation sequence, which pi doesn't send
-      bind-key -n S-Enter send-keys "\e[13;2u"
+      bind-key -n S-Enter send-keys "\e[13;3u"
       
       set -g status-left '[#S] #h '
       set -g status-left-length 20
