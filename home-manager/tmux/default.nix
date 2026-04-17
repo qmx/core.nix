bb6c719166a2e2f39dfa3c52e69bddc71b6f8d8a
Doug Campos <qmx@qmx.me>
2026-04-17 19:41:56 -0400

    feat(tmux): enable OSC 8 hyperlink passthrough
    Add hyperlinks to terminal-features so tmux renders OSC 8 escape
    sequences as clickable links instead of plain text.

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -11,8 +11,8 @@
       set -g extended-keys-format csi-u
       set -g allow-passthrough on
       
-      # Tell tmux that Ghostty (xterm-kitty) supports extended keys
-      set -g terminal-features ',xterm-kitty:extkeys'
+      # Tell tmux that Ghostty (xterm-kitty) supports extended keys and OSC 8 hyperlinks
+      set -g terminal-features ',xterm-kitty:extkeys,hyperlinks'
       
       set -g status-left '[#S] #h '
       set -g status-left-length 20
